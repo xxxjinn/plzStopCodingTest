5dde02e3e53dd5eca35703d149c3eb789377bfeb
xxxjinn <tlsguswls319@naver.com>
2024-09-24 19:23:32 +0900

    [BOJ]/14916/실5-return 추가

--- BOJ/[BOJ]_14916.js
+++ BOJ/[BOJ]_14916.js
@@ -7,7 +7,10 @@ let input = +fs.readFileSync(path).toString().trim();
 let answer = 0;
 
 //-1이 나오는 경우는 1 또는 3일 때 밖에 없다
-if (input === 1 || input === 3) console.log(-1);
+if (input === 1 || input === 3) {
+  console.log(-1);
+  return;
+}
 
 while (input > 0) {
   //숫자가 홀수이거나 5로 나눠떨어진다면
