5e2103933a211056fc39cc5a567a6e31d600c8ee
xxxjinn <tlsguswls319@naver.com>
2025-04-07 15:28:01 +0900

    [PGS]/12977/lv1

---
+++ PGS/[PGS]_12977.js
@@ -0,0 +1,32 @@
+// 소수 만들기
+
+function solution(nums) {
+  nums = nums.sort((a, b) => a - b);
+  let arr = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let fir = nums[i];
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      let sec = nums[j];
+      for (let k = j + 1; k < nums.length; k++) {
+        let thr = nums[k];
+        let sum = fir + sec + thr;
+        arr.push(sum);
+      }
+    }
+  }
+
+  let answer = arr.length;
+
+  for (let i = 0; i < arr.length; i++) {
+    let value = arr[i];
+    for (let j = 2; j <= value; j++) {
+      if (value !== j && value % j === 0) {
+        answer--;
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
