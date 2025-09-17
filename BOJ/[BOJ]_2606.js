fb0f2a475b38054d5f9c7f667108935b81ff5dd3
xxxjinn <tlsguswls319@naver.com>
2025-09-17 15:43:43 +0900

    [BOJ]/2606/실3

---
+++ BOJ/[BOJ]_2606.js
@@ -0,0 +1,46 @@
+//바이러스 - 실버3
+//못풂
+//그래프 연결은 생각했는데 정작 중요한 dfs 아이디어를 생각해내지 못함
+
+let fs = require("fs");
+let path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [comNum, pairNum, ...input] = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split("\n");
+
+comNum = Number(comNum);
+pairNum = Number(pairNum);
+
+let count = 0;
+let graph = [[]];
+
+for (let i = 0; i < pairNum; i++) {
+  let [n, m] = input[i].split(" ").map(Number);
+
+  if (!graph[n]) {
+    graph[n] = [];
+  }
+  if (!graph[m]) {
+    graph[m] = [];
+  }
+  graph[n].push(m);
+  graph[m].push(n);
+}
+
+let visited = Array.from(comNum + 1).fill(false);
+
+function dfs(start) {
+  visited[start] = true;
+
+  for (let next of graph[start]) {
+    if (!visited[next]) {
+      count++;
+      dfs(next);
+    }
+  }
+}
+dfs(1);
+
+console.log(count);
