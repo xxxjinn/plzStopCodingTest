19478ebc3838dfa3b094904eb8ff0fa26dfb8072
xxxjinn <tlsguswls319@naver.com>
2025-10-01 16:38:34 +0900

    [BOJ]/2941/실5

--- BOJ/[BOJ]_2941.js
+++ BOJ/[BOJ]_2941.js
@@ -1,11 +1,15 @@
 //크로아티아 알파벳
+//실버5
+//10분 소요
+
 let fs = require("fs");
 let path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let str = fs.readFileSync(path).toString().trim();
 
-let alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+const alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+//dz=랑 z= 순서가 바뀌면 dz=가 d+z=로 잘릴 수 있기 때문에 주의해야 함!
 
-for (let v of alphabet) {
-  str = str.replaceAll(v, "*");
+for (const a of alphabet) {
+  str = str.replaceAll(a, "*");
 }
 console.log(str.length);
