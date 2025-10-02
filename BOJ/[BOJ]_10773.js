c9146fa7b467121cc4161d3d6cb81ef1aa903f47
xxxjinn <tlsguswls319@naver.com>
2025-10-02 16:35:05 +0900

    [BOJ]/10773/실4

--- BOJ/[BOJ]_10773.js
+++ BOJ/[BOJ]_10773.js
@@ -3,25 +3,22 @@
 
 let fs = require("fs");
 let path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let [_, ...input] = fs
+let [k, ...input] = fs
   .readFileSync(path)
   .toString()
   .trim()
   .split("\n")
   .map(Number);
 
-let arr = [];
-for (let i = 0; i < input.length; i++) {
-  let currentNumber = input[i];
+let sum = [];
 
-  if (currentNumber !== 0) {
-    arr.push(currentNumber);
-  } else {
-    arr.pop();
+for (let i = 0; i < k; i++) {
+  let number = input[i];
+  if (number !== 0) {
+    sum.push(number);
+    continue;
   }
+  sum.pop();
 }
 
-console.log(arr.reduce((pre, cur) => pre + cur, 0));
-
-//문제 난이도가 잘못 배정된 것 같다,,
-//실버3을 못풀어내서 실버4를 골랐는데 너무 쉬워서 실버3을 계속 풀어야겠음!
+console.log(sum.reduce((pre, cur) => pre + cur, 0));
