d4eb6ca3ca48a00c58c2468a0767e8b3940e9f84
xxxjinn <tlsguswls319@naver.com>
2025-10-10 23:20:44 +0900

    [BOJ]/10828/실4

--- BOJ/[BOJ]_10828.js
+++ BOJ/[BOJ]_10828.js
@@ -1,52 +1,28 @@
 //스택 - 실버4
-//30분 소요
-//로직은 잘 풀었는데 시간초과 -> console.log를 매번 찍어주면 안됨 => 모아서 한 번에 나중에 출력하는 방식으로 해결
+//15분 소요
 
 let fs = require("fs");
 let path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let [length, ...input] = fs.readFileSync(path).toString().split("\n");
+let [n, ...input] = fs.readFileSync(path).toString().trim().split("\n");
 
-let stack = [];
 let result = [];
+let stack = [];
 
-const push = (number) => {
-  stack.push(number);
-};
+for (let i = 0; i < n; i++) {
+  let [command, number] = input[i].split(" ");
 
-const top = () => {
-  return stack[stack.length - 1] ? stack[stack.length - 1] : -1;
-};
-
-const size = () => {
-  return stack.length;
-};
-
-const empty = () => {
-  return stack.length === 0 ? 1 : 0;
-};
-
-const pop = () => {
-  return stack.length === 0 ? -1 : stack.pop();
-};
-
-const main = () => {
-  for (let i = 0; i < length; i++) {
-    let [command, number] = input[i].split(" ");
-
-    if (command === "push") {
-      push(number);
-    } else if (command === "top") {
-      result.push(top());
-    } else if (command === "pop") {
-      result.push(pop());
-    } else if (command === "empty") {
-      result.push(empty());
-    } else if (command === "size") {
-      result.push(size());
-    }
+  if (command === "push") {
+    stack.push(number);
+  } else if (command === "top") {
+    result.push(stack[stack.length - 1] ? stack[stack.length - 1] : -1);
+  } else if (command === "pop") {
+    const pop = stack.pop();
+    result.push(pop ? pop : -1);
+  } else if (command === "size") {
+    result.push(stack.length);
+  } else if (command === "empty") {
+    result.push(stack.length === 0 ? 1 : 0);
   }
+}
 
-  console.log(result.join("\n"));
-};
-
-main();
+console.log(result.join("\n"));
