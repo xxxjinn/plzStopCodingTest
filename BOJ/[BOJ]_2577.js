4ca438dbc6a381e067727b583dcb005060c07579
xxxjinn <tlsguswls319@naver.com>
2026-01-17 13:10:05 +0900

    [BOJ]/2577/브2

--- BOJ/[BOJ]_2577.js
+++ BOJ/[BOJ]_2577.js
@@ -1,17 +1,18 @@
-//숫자의 개수
-//브론즈2
-//10분 미만 소요
+//숫자의 개수 - 브2
+//4분 소요
 
 let fs = require("fs");
 let path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(path).toString().trim().split("\n").map(Number);
+let [a, b, c] = fs.readFileSync(path).toString().trim().split("\n").map(Number);
 
-let cal = (input[0] * input[1] * input[2]).toString();
-let obj = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 };
+let res = (a * b * c).toString();
+let arr = [];
 
-for (let i = 0; i < cal.length; i++) {
-  let char = cal[i];
-  obj[char] = obj[char] + 1;
+for (let i = 0; i <= 9; i++) {
+  arr[i] = res
+    .split("")
+    .map(Number)
+    .filter((v) => v === i).length;
 }
 
-console.log(Object.values(obj).join("\n"));
+console.log(arr.join("\n"));
