34796c1ee042b098a37ba36b56bb9a5f5c43a44a
xxxjinn <tlsguswls319@naver.com>
2026-07-03 15:54:37 +0900

    solve:12941

---
+++ PGS/lv2/12941.js
@@ -0,0 +1,16 @@
+//최솟값 만들기
+
+//6분 소요
+
+function solution(A, B) {
+  let sum = 0;
+
+  A = A.sort((a, b) => a - b);
+  B = B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    sum += A[i] * B[i];
+  }
+
+  return sum;
+}
